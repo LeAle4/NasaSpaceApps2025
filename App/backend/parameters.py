61f81a11b87ead02f14e09d4c6e0050b2d5dec82
domigc <domigc@outlook.cl>
2025-10-04 15:09:16 -0300

    Parametros añadidos

---
+++ App/backend/parameters.py
@@ -0,0 +1,10 @@
+DATA_HEADERS = ['ra', 'dec', 'koi_gmag', 'koi_count', 'koi_num_transits',
+       'koi_max_mult_ev', 'koi_bin_oedp_sig', 'koi_ldm_coeff1',
+       'koi_model_snr', 'koi_prad', 'koi_impact', 'koi_duration', 'koi_depth',
+       'koi_dor', 'koi_incl', 'koi_teq', 'koi_steff', 'koi_slogg', 'koi_smet',
+       'koi_srad', 'koi_smass', 'koi_fwm_stat_sig', 'koi_fwm_srao',
+       'koi_fwm_sdeco', 'koi_fwm_prao', 'koi_fwm_pdeco', 'koi_dicco_mra',
+       'koi_dicco_mdec', 'koi_dicco_msky', 'koi_tce_plnt_num', 'koi_time0',
+       'koi_fpflag_nt', 'koi_fpflag_ss', 'koi_fpflag_co', 'koi_fpflag_ec',
+       'koi_insol', 'koi_srho']
+#hola
